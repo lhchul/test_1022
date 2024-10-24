22b1d76c280c6c85734eab93c7b68f44b4dbb350
이형출 <lhchul@nate.com>
2024-10-24 08:30:33 +0000

    1730

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,14 @@ def find_nanum_font():
     system = platform.system()
     font_path = None
 
-    if system == "Windows":
-        font_path = r"C:\Windows\Fonts\NanumGothic.ttf"
-    elif system == "Linux":
-        font_path = "/usr/share/fonts/truetype/nanum/NanumGothic.ttf"
-    elif system == "Darwin":  # MacOS
-        font_path = "/Library/Fonts/NanumGothic.ttf"
+  #  if system == "Windows":
+  #     font_path = r"C:\Windows\Fonts\NanumGothic.ttf"
+  #  elif system == "Linux":
+  #      font_path = "/usr/share/fonts/truetype/nanum/NanumGothic.ttf"
+  #  elif system == "Darwin":  # MacOS
+  #      font_path = "/Library/Fonts/NanumGothic.ttf"
+    
+    font_path ="NanumGothic.ttf"
 
     # 폰트가 없을 경우 자동 검색
     if not font_path or not os.path.exists(font_path):
